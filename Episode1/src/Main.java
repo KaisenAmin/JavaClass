661945a8c348b780253b28aaa37e31f050b5bf29
amin tahmasebi <amintahmasebi479@gmail.com>
2023-08-06 13:26:51 +0330

    episode1 java tutorial

---
+++ Episode1/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World\n");
+        System.out.println("Hello Java Programmers");
+        System.out.println("Java is good");
+
+        System.out.println(156);
+        System.out.print("man halam khobe " + " to chetor?" + " lotfan bego " + 20 + "\n");
+        System.out.println("The number is 20" + "goood");
+        System.out.println();
+        System.out.print("C++ Programmers\n");
+        System.out.print("Python programmers\n");
+
+        System.out.printf("Hello %s\n", "amin");
+        System.out.printf("Your age is %d\n", 52);
+        System.out.printf("The grad is %f\n", 15.326);
+        System.out.printf("Your gender is %c\n", 'f');
+        System.out.printf("The boolean value is %b\n", false);
+        System.out.printf("Hex of 1555 %x\n", 1555);
+        System.out.printf("Oct of 1587 is %o\n", 1587);
+        System.out.printf("%s\n", Integer.toBinaryString(15));
+    }
+}
